d13519e0f1f41ee442d3529ef456219e3a2db7cb
Xu Donghui <donghui_xu@hotmail.com>
2015-10-14 00:14:15 +0800

    Add a second storyboard

---
+++ BoBanTang/BBTMarco.h
@@ -0,0 +1,18 @@
+//
+//  BBTMarco.h
+//  BoBanTang
+//
+//  Created by Hsu Tung Hui on 13/10/15.
+//  Copyright © 2015年 BBT. All rights reserved.
+//
+
+#ifndef BBTMarco_h
+#define BBTMarco_h
+
+typedef NS_ENUM(NSInteger, SCUTCampus) {
+    SCUTCampusNorth,
+    SCUTCampusHEMC
+};
+
+
+#endif /* BBTMarco_h */
