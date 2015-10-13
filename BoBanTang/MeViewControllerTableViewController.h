aed96730a5113befca7317aad5012b63af5501a6
Caesar <pankobe24@vip.qq.com>
2015-10-14 00:11:07 +0800

    Reorganize the hierarchy and use storyboard references to split storyboards

---
+++ BoBanTang/MeViewControllerTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MoreViewControllerTableViewController.h
+//  BoBanTang
+//
+//  Created by Caesar on 15/10/13.
+//  Copyright © 2015年 BBT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//“更多”页面对应的viewController
+@interface MeViewControllerTableViewController : UITableViewController
+
+@end
+    
