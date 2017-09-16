7c66e850302bc5d28d81c9c2e721b4ddbbd9ad94
权融威 <370850377@qq.com>
2017-09-16 14:04:50 +0800

    增加了首页课表和社团的登录判断，搭建了 我 -> 个人资料页面的UI

---
+++ BoBanTang/BBTProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBTProfileViewController.h
+//  波板糖
+//
+//  Created by Authority on 2017/9/15.
+//  Copyright © 2017年 100steps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBTProfileViewController : UIViewController
+
+@end
